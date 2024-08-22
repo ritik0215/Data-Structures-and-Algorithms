24d41aa3e39d9b48ab9976aaf1c0e20bf46a2d24
Vishwajeet Singh <118260556+ritik0215@users.noreply.github.com>
2024-08-23 03:19:08 +0530

    Time: 4 ms (71.86%), Space: 15.6 MB (81.79%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -16,25 +16,25 @@ public:
     //     return dp[i][buy];
         
        
-    // }
-    int solveTab(vector<int>&prices){
-        int n=prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2,0));
-        int profit;
-        for(int i=n-1;i>=0;i--){
-            for(int j=0;j<2;j++){
+    // // }
+    // int solveTab(vector<int>&prices){
+    //     int n=prices.size();
+    //     vector<vector<int>> dp(n+1,vector<int>(2,0));
+    //     int profit;
+    //     for(int i=n-1;i>=0;i--){
+    //         for(int j=0;j<2;j++){
                 
-                if(j==0){
-                   profit=max(0+dp[i+1][0],-prices[i]+dp[i+1][1]);
-                }
-                if(j==1){
-                   profit= max(0+dp[i+1][1],prices[i]+dp[i+1][0]);
-                }
-                dp[i][j]= profit;
-            }            
-        }
-        return dp[0][0];
-    }
+    //             if(j==0){
+    //                profit=max(0+dp[i+1][0],-prices[i]+dp[i+1][1]);
+    //             }
+    //             if(j==1){
+    //                profit= max(0+dp[i+1][1],prices[i]+dp[i+1][0]);
+    //             }
+    //             dp[i][j]= profit;
+    //         }            
+    //     }
+    //     return dp[0][0];
+    // }
     int maxProfit(vector<int>& prices) {
         // int n=prices.size();
         // vector<vector<int>> dp(n,vector<int>(2,-1));
