4cfa9ebf25447de4fb54619a6d5fc07c9d47d6a6
Vishwajeet Singh <118260556+ritik0215@users.noreply.github.com>
2024-08-25 22:44:26 +0530

    Time: 3 ms (48.26%), Space: 11.1 MB (27.01%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
        int n=citations.size();
-       if(n==1)return citations[0]!=0;
+      
         sort(citations.begin(),citations.end());
         
         
